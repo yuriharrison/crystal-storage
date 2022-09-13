5211a344beae38706e52a0b4a013f4c1c86bdef6
Yuri Harrison <yuri.harrison15@gmail.com>
2022-09-13 18:52:44 -0300

    add io serializer

--- spec/byte_serializer_spec.cr
+++ spec/byte_serializer_spec.cr
@@ -1,9 +1,91 @@
 require "./spec_helper"
 
-describe Serializer do
-  it "#to_io" do
-    io = IO::Memory.new
+def rand_string(size)
+  Array.new(size) { rand(32...126).unsafe_as(Char) }
+    .join " "
+end
+
+
+module TestModule(T)
+  getter name
+  getter age
+  getter points
+  getter pets
+  getter non_serializable_field
+
+  macro included
+    @[Field(ignore: true)]
+    getter non_serializable_field : Bool = false
+
+    def self.gen_random
+      T.new \
+        rand_string(10),
+        rand(Int8),
+        rand(Int32),
+        Array.new 10 { rand_string 10 },
+        non_serializable_field: true
+    end
+  end
+
+  def initialize(@name : String, @age : Int8, @points : Int32, @pets : Array(String), @non_serializable_field=false)
+  end
+
+  def ==(other : T)
+    @name == other.name && 
+      @age == other.age && 
+      @points == other.points &&
+      @pets == other.pets
+  end
+end
+
+struct TestStruct
+  include Serializable
+  include TestModule(TestStruct)
+end
+
+class TestClass
+  include Serializable
+  include TestModule(TestClass)
+end
+
+
+describe Serializable do
+  it "String #to_io #from_io" do
     value = "test"
+    io = IO::Memory.new
     value.to_io io
+    String.from_io(io.rewind).should eq value
+  end
+
+  it "Array(Float) #to_io #from_io" do
+    array = Array(Float32).new 10 { rand(Float32::MAX) }
+    io = IO::Memory.new
+    array.to_io io
+    Array(Float32).from_io(io.rewind).should eq array
+  end
+
+  it "Array(String) #to_io #from_io" do
+    array = Array(String).new 10 { rand_string 100 }
+    io = IO::Memory.new
+    array.to_io io
+    Array(String).from_io(io.rewind).should eq array
+  end
+
+  it "Struct TestStruct #to_io #from_io" do
+    value = TestStruct.gen_random
+    io = IO::Memory.new
+    value.to_io io
+    value_from = TestStruct.from_io(io.rewind)
+    value_from.should eq value
+    value_from.non_serializable_field.should be_falsey
+  end
+
+  it "Class TestClass #to_io #from_io" do
+    value = TestClass.gen_random
+    io = IO::Memory.new
+    value.to_io io
+    value_from = TestClass.from_io(io.rewind)
+    value_from.should eq value
+    value_from.non_serializable_field.should be_falsey
   end
 end
--- src/byte_serializer.cr
+++ src/byte_serializer.cr
@@ -1,20 +1,69 @@
+class Array(T)
+  def self.from_io(io : IO, format=nil)
+    Array.new io.read_bytes(Int32) do
+      io.read_bytes T
+    end
+  end
+  
+  def to_io(io : IO, format=nil)
+    io.write_bytes @size
+    each do |item|
+      io.write_bytes item
+    end
+    io
+  end
+end
+
 class String
-  def self.from_io(io : IO)
-    io.read_string io.write_bytes Int32
+  def self.from_io(io : IO, format=nil)
+    io.read_string io.read_bytes Int32
   end
 
-  def to_io(io : IO)
-    io.write_bytes bytesize
+  def to_io(io : IO, format=nil)
     io.write_bytes bytes
     io
   end
 end
 
-module Serializer
+module Serializable
   annotation Field
   end
+  
+  macro included
+    private def self.new_from_io(io : IO)
+      instance = allocate
+      instance.initialize(__from_io: io)
+      GC.add_finalizer(instance) if instance.responds_to?(:finalize)
+      instance
+    end
 
-  def to_io(io : IO)
+    def self.new(io : IO)
+      new_from_io(io)
+    end
+
+    def self.from_io(io : IO, format=nil)
+      new io
+    end
+
+    macro inherited
+      def self.new(io : IO)
+        new_from_io(io)
+      end
+    end
+  end
+
+  def initialize(*, __from_io io : IO)
+    {% begin %}
+      {% for ivar in @type.instance_vars %}
+        {% ann = ivar.annotation(Field) %}
+        {% unless ann && (ann[:ignore] || ann[:ignore_deserialize]) %}
+        @{{ ivar.id }} = io.read_bytes {{ ivar.type }}
+        {% end %}
+      {% end %}
+    {% end %}
+  end
+
+  def to_io(io : IO, format=nil)
     {% begin %}
       {% for ivar in @type.instance_vars %}
         {% ann = ivar.annotation(Field) %}
